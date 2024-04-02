39b9cb6600021571c4cb44e50e3ee58fd5462c0c
romanvfe <roman.v@mpsolutions.kz>
2024-04-02 17:35:09 +0600

    IE-6: First Commit

--- config/constants.ts
+++ config/constants.ts
@@ -1,17 +1,1 @@
 export const HeaderID: string = 'header';
-
-/**
- * Код достурных локалей
- */
-export const localeCode = {
-    RU: 'ru',
-    EN: 'en',
-};
-
-/**
- * Наименование языка по коду
- */
-export const localeName = {
-    [localeCode.RU]: 'Русский',
-    [localeCode.EN]: 'English'
-};
--- config/login.ts
+++
@@ -1,9 +0,0 @@
-export const loginViewState = {
-    PREVIEW: 1,
-    FORM:    2,
-};
-
-export const loginFormViewState = {
-    EMAIL:    1,
-    PASSWORD: 2,
-};
