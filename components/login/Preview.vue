0665b7c8eb8f34143a0848e208c07e7e9c0f4e1b
romanvfe <roman.v@mpsolutions.kz>
2024-04-03 09:22:21 +0600

    IE-7: Login Page - Экран preview

---
+++ components/login/Preview.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import UiButton from '@/components/ui/Button.vue';
+
+const { t } = useI18n();
+</script>
+
+<template>
+    <div class="login-preview">
+        <p class="login-preview__desc">
+            {{ t('login_page.preview_description') }}
+        </p>
+
+        <UiButton
+            type="button"
+            class="login-preview__btn"
+        >
+            {{ t('ui.continue') }}
+        </UiButton>
+    </div>
+</template>
+
+<style lang="scss">
+.login-preview {
+    position: relative;
+
+    &__desc {
+        margin-bottom: 16px;
+        font-size: 16px;
+        font-weight: var(--f-weight-regular);
+        line-height: 24px;
+        color: var(--text-color);
+    }
+}
+</style>
