24ee3cb4134fa615ae7ea966112e4c04284e6f91
romanvfe <roman.v@mpsolutions.kz>
2024-04-03 12:42:42 +0600

    IE-9: LoginPage: Добавить ссылка на компанию разработчик

--- pages/login.vue
+++ pages/login.vue
@@ -23,6 +23,14 @@ definePageMeta({
 
             <LoginPreview />
         </div>
+
+        <a
+            href="https://ar-tech.group/ru"
+            class="login-page__dev-link"
+            target="_blank"
+        >
+            Powered By AR Tech Group
+        </a>
     </div>
 </template>
 
@@ -70,6 +78,30 @@ definePageMeta({
         img {
             width: 100%;
             max-width: 180px;
+        }
+    }
+
+    &__dev-link {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 24px;
+        display: block;
+        width: 100%;
+        font-size: 16px;
+        font-weight: var(--f-weight-regular);
+        line-height: 24px;
+        text-align: center;
+        text-decoration: none;
+        color: var(--orange);
+        transition: color 0.5s ease;
+
+        &:hover {
+            color: var(--orange-light);
+        }
+
+        @media (min-width: 1200px) {
+            bottom: 36px;
         }
     }
 }
